6d02bc8e57b20e404b12d66f9d016c08322c0d89
Alec Budd <alec.budd@prodyna.com>
2021-01-08 15:31:20 +0100

    strapi test

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,21 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+      <div>Hello</div>
     </v-col>
   </v-row>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  mounted() {
+    this.$nextTick(function () {
+      axios
+        .get('http://localhost:1337/posts')
+        .then((res) => console.log(res.data))
+    })
+  },
+}
+</script>
